4a9a82c34b4b27876a0b6df5246796deadc5c78c
zxfjd3g <258147149@qq.com>
2018-12-22 16:38:36 +0800

    Star组件

--- src/components/ShopList/ShopList.vue
+++ src/components/ShopList/ShopList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="shop_container">
-    <ul class="shop_list">
+    <ul class="shop_list" v-if="shops.length>0">
       <li class="shop_li border-1px" v-for="(shop, index) in shops" :key="index">
         <a>
           <div class="shop_left">
@@ -15,13 +15,7 @@
             </section>
             <section class="shop_rating_order">
               <section class="shop_rating_order_left">
-                <div class="star star-24">
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item on"></span>
-                  <span class="star-item half"></span>
-                  <span class="star-item off"></span>
-                </div>
+                <Star :score="shop.rating" :size="24"/>
                 <div class="rating_section">
                   {{shop.rating}}
                 </div>
@@ -42,6 +36,20 @@
             </section>
           </div>
         </a>
+      </li>
+    </ul>
+    <ul v-else>
+      <li>
+        <img src="./images/shop_back.svg" alt="loading">
+      </li>
+      <li>
+        <img src="./images/shop_back.svg" alt="loading">
+      </li>
+      <li>
+        <img src="./images/shop_back.svg" alt="loading">
+      </li>
+      <li>
+        <img src="./images/shop_back.svg" alt="loading">
       </li>
     </ul>
   </div>
@@ -127,54 +135,6 @@
               .shop_rating_order_left
                 float left
                 color #ff9a0d
-                .star //2x图 3x图
-                  float left
-                  font-size 0
-                  .star-item
-                    display inline-block
-                    background-repeat no-repeat
-                  &.star-48
-                    .star-item
-                      width 20px
-                      height 20px
-                      margin-right 22px
-                      background-size 20px 20px
-                      &:last-child
-                        margin-right: 0
-                      &.on
-                        bg-image('./images/stars/star48_on')
-                      &.half
-                        bg-image('./images/stars/star48_half')
-                      &.off
-                        bg-image('./images/stars/star48_off')
-                  &.star-36
-                    .star-item
-                      width 15px
-                      height 15px
-                      margin-right 6px
-                      background-size 15px 15px
-                      &:last-child
-                        margin-right 0
-                      &.on
-                        bg-image('./images/stars/star36_on')
-                      &.half
-                        bg-image('./images/stars/star36_half')
-                      &.off
-                        bg-image('./images/stars/star36_off')
-                  &.star-24
-                    .star-item
-                      width 10px
-                      height 10px
-                      margin-right 3px
-                      background-size 10px 10px
-                      &:last-child
-                        margin-right 0
-                      &.on
-                        bg-image('./images/stars/star24_on')
-                      &.half
-                        bg-image('./images/stars/star24_half')
-                      &.off
-                        bg-image('./images/stars/star24_off')
                 .rating_section
                   float left
                   font-size 10px
--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 
 import NavHeader from './components/NavHeader/NavHeader.vue'
+import Star from './components/Star/Star.vue'
+
 
 // 注册全局组件
 Vue.component('NavHeader', NavHeader)
+Vue.component('Star', Star)
 
 /* eslint-disable no-new */
 new Vue({
