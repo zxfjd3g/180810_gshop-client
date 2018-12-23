2836e9239cf2eee65823a56c85eba0c8fe8d92b9
zxfjd3g <258147149@qq.com>
2018-12-23 11:26:16 +0800

    login组件

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -32,10 +32,10 @@
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isShowPwd ? 'text' : 'password'" maxlength="8" placeholder="密码">
+                <div class="switch_button" :class="isShowPwd ? 'on' : 'off'" @click="isShowPwd=!isShowPwd">
+                  <div class="switch_circle" :class="{right: isShowPwd}"></div>
+                  <span class="switch_text">{{isShowPwd ? 'abc' : ''}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -62,6 +62,7 @@
         loginWay: true, // true: 短信, false: 密码
         phone: '', // 手机号
         computeTime: 0, // 倒计时剩余的时间, 单位秒
+        isShowPwd: false, // 是否显示密码
       }
     },
 
@@ -182,7 +183,6 @@
                 &.on
                   background #02a774
                 >.switch_circle
-                //transform translateX(27px)
                   position absolute
                   top -1px
                   left -1px
@@ -193,6 +193,8 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
             .login_hint
               margin-top 12px
               color #999
